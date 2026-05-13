b341044ace3cc03e494babd7b942ba0e8ae2c133
byteAndBait <abdulrahman.elkassas1212@gmail.com>
2026-05-13 12:53:45 +0300

    feat: Add linting and formatting + fix some issues

--- webpack.common.js
+++ webpack.common.js
@@ -2,26 +2,24 @@
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
-   plugins: [
-     new HtmlWebpackPlugin({
-            template:"./src/index.html",
-            title: 'Production'
-        }),
-   ],
-   module:{
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Production",
+    }),
+  ],
+  module: {
     rules: [
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }
-  ]
-   }
-    
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
